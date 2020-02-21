9dac107f91b52d10e590dfb1c71b0b1e1c967945
robbmue <rom@fedora.netzlink.com>
2020-02-21 18:38:15 +0100

    and god said, i'd be light

--- cmd/test.go
+++ cmd/test.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -14,7 +15,7 @@ func main() {
 	x.Ping()
 	reader := bufio.NewReader(os.Stdin)
 	for{
-		fmt.Println("Sunblinds [TOP(0)|UP(1)|DOWN(2)|BOTTOM(3)]")
+		fmt.Println("Sunblinds [TOP(0)|UP(1)|DOWN(2)|BOTTOM(3)|CUSTOM(4)|DISCO(5)]")
 		input, _, _ := reader.ReadLine()
 		params := make([]int, 3)
 		i,_ := strconv.Atoi(string(input))
@@ -25,6 +26,14 @@ func main() {
 				params[i],_ = strconv.Atoi(x)
 			}
 
+		}
+		if i == 5{
+			for {
+				x.Light(1,100)
+				time.Sleep(time.Second)
+				x.Light(0,0)
+				time.Sleep(time.Second)
+			}
 		}
 		x.Sunblind(client.Direction(i), params[0], params[1], params[2])
 	}
